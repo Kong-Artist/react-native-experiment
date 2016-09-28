d00642e90e2e756bfde1071d9c8ddf87de2b3b2d
Steven Kong <stevendjkong@gmail.com>
2016-09-28 17:05:54 -0400

    Multi component Angular 2 app with services and routing

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,8 +6,12 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
   getHeroes(): Promise<Hero[]> {
-    return new Promise<Hero[]>(resolve =>
-    setTimeout(resolve, 2000)) // delay 2 seconds
-    .then(() => HEROES);
+    return Promise.resolve(HEROES);
   }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+              .then(heroes => heroes.find(hero => hero.id === id));
+  }
+
 }
